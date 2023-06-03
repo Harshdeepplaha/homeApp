99ad0726e59843a55df73c33ea52f9ba51281e59
Harshdeep Singh Plaha <75137919+Harshdeepplaha@users.noreply.github.com>
2023-06-03 15:04:24 +0530

    dashboard updates

---
+++ src/components/Accordian.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import {collapse} from 'accordion-collapse-react-native'
+import 
+
+export default function Accordian() {
+  return (
+    <View>
+      <Text>Accordian</Text>
+      
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
